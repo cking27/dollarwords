ee605d311923e88a9833bffa8dbf2fe3380bd614
Charlie King <charlie.king@dimensional.com>
2019-08-08 15:10:53 -0500

    first cut

---
+++ DollarWords/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DollarWordsNs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var dollarWords = new DollarWords();
+
+            var wordList = new[]
+            {
+                "job",
+                "Practice",
+                "ironized",
+                "non-stop"
+            };
+
+            var results = dollarWords.GetResults(wordList);
+
+            foreach (var result in results)
+            {
+                // should print "ironized"
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    public class DollarWords
+    {
+        public IEnumerable<string> GetResults(IEnumerable<string> wordList)
+        {
+            // TODO: Implement solution here
+
+        }
+    }
+}
